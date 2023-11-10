b73a46e137601c1c73f74a7cfae9fe8a21728699
Lāsma Pokšāne <laasmapoksane@gmail.com>
2023-11-11 00:11:10 +0100

    add routing and render Login and SignUp

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { Routes, Route } from "react-router-dom";
 import Hero from "./components/Hero/Hero";
+import SignUp from "./containers/SignUp/SignUp";
+import Login from "./containers/Login/Login";
 
 const App = () => {
-  return <Hero />;
+  return (
+    <Routes>
+      <Route path="/" element={<Hero />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
 };
 
 export default App;
