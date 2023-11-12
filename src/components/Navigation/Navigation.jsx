e96379df0d63453957dbab3a5fcc190743c4aa68
Lāsma Pokšāne <laasmapoksane@gmail.com>
2023-11-12 12:08:33 +0100

    add hamburger icon that can be toggled

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 
 const Navigation = ({ setIsAuthenticated }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const isAuthenticated = localStorage.getItem("token") !== null;
 
   const navigate = useNavigate();
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   const logoutHandler = () => {
     try {
@@ -100,6 +106,34 @@ const Navigation = ({ setIsAuthenticated }) => {
                   </div>
                 </div>
               </div>
+            </div>
+            <div className="md:hidden flex items-center">
+              <button onClick={toggleMenu} className="text-gray-100">
+                <svg
+                  className="h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  {isMenuOpen ? (
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  ) : (
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  )}
+                </svg>
+              </button>
             </div>
           </div>
           <div className="md:hidden absolute top-16 left-0 right-0 bg-purple-800 dark:bg-indigo-800">
