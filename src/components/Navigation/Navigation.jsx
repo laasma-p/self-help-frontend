631d0c3749c5e68b674f006790fe67303478306a
Lāsma Pokšāne <laasmapoksane@gmail.com>
2023-11-14 13:24:59 +0100

    add Skills link to navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -80,6 +80,16 @@ const Navigation = ({ setIsAuthenticated }) => {
                   <div className="opacity-0 invisible absolute transition-all duration-300 transform translate-y-2 right-0 z-50 group-hover:opacity-100 group-hover:visible group-hover:translate-y-0">
                     <div className="bg-purple-800 dark:bg-indigo-800 shadow-lg dark:border-gray-700 divide-y divide-gray-100 dark:divide-gray-100 w-48">
                       <NavLink
+                        to="/values"
+                        className={({ isActive }) =>
+                          isActive
+                            ? "px-4 py-2 text-gray-100 dark:text-gray-100 bg-purple-400 dark:bg-indigo-400 block w-full text-left"
+                            : "px-4 py-2 text-gray-100 dark:text-gray-100 hover:bg-purple-400 dark:hover:bg-indigo-400 block w-full text-left"
+                        }
+                      >
+                        Values
+                      </NavLink>
+                      <NavLink
                         to="/boundaries"
                         className={({ isActive }) =>
                           isActive
@@ -90,14 +100,14 @@ const Navigation = ({ setIsAuthenticated }) => {
                         Boundaries
                       </NavLink>
                       <NavLink
-                        to="/values"
+                        to="/skills"
                         className={({ isActive }) =>
                           isActive
                             ? "px-4 py-2 text-gray-100 dark:text-gray-100 bg-purple-400 dark:bg-indigo-400 block w-full text-left"
                             : "px-4 py-2 text-gray-100 dark:text-gray-100 hover:bg-purple-400 dark:hover:bg-indigo-400 block w-full text-left"
                         }
                       >
-                        Values
+                        Skills
                       </NavLink>
                       <NavLink
                         to="/"
@@ -208,6 +218,17 @@ const Navigation = ({ setIsAuthenticated }) => {
                   }
                 >
                   Boundaries
+                </NavLink>
+                <NavLink
+                  to="/skills"
+                  onClick={closeMenu}
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-gray-100 dark:text-gray-100 bg-purple-400 dark:bg-indigo-400 block px-3 py-2 rounded-md text-base font-medium"
+                      : "text-gray-100 dark:text-gray-100 block px-3 py-2 rounded-md text-base font-medium"
+                  }
+                >
+                  Skills
                 </NavLink>
                 <NavLink
                   to="/"
