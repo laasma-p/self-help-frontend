af502dbbaf4389cf94ae7dba051d3b68050eff87
Lāsma Pokšāne <laasmapoksane@gmail.com>
2023-11-14 13:26:24 +0100

    create initial Skills styled with TailwindCSS

---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,9 @@
+const Skills = () => {
+  return (
+    <div className="min-h-screen pb-6 flex flex-col items-center bg-gray-100 dark:bg-neutral-700 text-neutral-700 dark:text-gray-100 transition-colors duration-300">
+      <h1 className="text-3xl mt-8 mb-6 md:text-4xl text-center">Skills</h1>
+    </div>
+  );
+};
+
+export default Skills;
