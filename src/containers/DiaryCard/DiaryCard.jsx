dd8be9c3a2a0fabc484ec157459d2944ab816967
Lāsma Pokšāne <laasmapoksane@gmail.com>
2023-12-10 23:46:40 +0100

    create initial DiaryCard styled with TailwindCSS

---
+++ src/containers/DiaryCard/DiaryCard.jsx
@@ -0,0 +1,11 @@
+const DiaryCard = () => {
+  return (
+    <div className="min-h-screen pb-6 flex flex-col items-center bg-gray-100 dark:bg-neutral-700 text-neutral-700 dark:text-gray-100 transition-colors duration-300">
+      <h1 className="text-3xl mt-8 mb-6 md:text-4xl text-center">
+        Diary Cards
+      </h1>
+    </div>
+  );
+};
+
+export default DiaryCard;
